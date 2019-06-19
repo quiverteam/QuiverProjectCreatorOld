383ca18ff0e3f14b56e71072ebce8515b8a24ca3
swissChili <swisschili@fastmail.com>
2019-06-19 09:43:00 -0700

    [kvmap] Added KeyValues to Base.Map function

--- src/kvmap.ml
+++ src/kvmap.ml
@@ -30,25 +30,49 @@ open Base
     means that the first occurance of a value assigned to a key in a certain
     block will be the last to be read from the Str variant, and at the end of
     the [string list]. *)
-type value = Str of string list
+type value = Str of string
            | Block of kvmap
 
 (** [kvmap] is the type of the interal OCaml representation of KeyValues, not as
     an AST like the one the parser creates, but rather as a map that can be
     easily used by other functions *)
-and kvmap = (string, value, String.comparator_witness) Map.t
+and kvmap = (string, value list, String.comparator_witness) Map.t
 
 let empty = Map.empty (module String)
 
-let insert map k v =
+let insert (map : kvmap) k v : kvmap =
     let data = match Map.find map k with
-        | Some x -> v :: x
+        | Some x -> x @ [v]
         | None -> [v] in
-    Map.add map ~key:k ~data:data
+    Map.set map ~key:k ~data:data
 
 let get_all map k = Map.find map k
 
+(** [get_one] is useful for extracting a value that is meant to be declared
+    exactly once in the KeyValues. It returns [Some value] if it is found
+    exactly once, [None] otherwise. *)
 let get_one map k : value option = match Map.find map k with
     | None -> None
     | Some (x :: _) -> Some x
     | Some [] -> None
+
+(** [bool_of_condition] evaluates a condition in a given context.
+    @argument [string list] defined
+    @argument [condition] conditions *)
+let rec bool_of_condition def = function
+    | And (a, b) -> (bool_of_condition def a) && (bool_of_condition def b)
+    | Or (a, b)  -> (bool_of_condition def a) || (bool_of_condition def b)
+    | Not a      -> not (bool_of_condition def a)
+    | Defined a  -> List.exists def ~f:(String.equal a)
+    | Empty      -> true
+
+let rec kvmap_of_syntax s def : kvmap = match s with
+    | [] -> empty
+    | (k, v, c) :: xs -> match bool_of_condition def c with
+        | false -> kvmap_of_syntax xs def
+        | true  ->
+            let map = kvmap_of_syntax xs def in
+            insert map k (match v with
+                | Str' a -> Str a
+                | Block' a -> Block (kvmap_of_syntax a def)
+            )
