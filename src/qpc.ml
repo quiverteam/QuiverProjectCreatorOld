8abf30cd06229bda48e2856ee94ace36be099d1c
swissChili <swisschili@fastmail.com>
2019-06-19 04:01:34 -0700

    [qpc] Added boilerplate main code

---
+++ src/qpc.ml
@@ -0,0 +1,29 @@
+(** Quiver Project Creator (or QPC) is a build tool inspired by Valve's VPC.
+    This repository contains both the QPC source code, and the KV1 parser
+    on which it depends. This source file is part of QPC, and is unrelated
+    to the KV1 parser.
+
+    Copyright (C) 2019  swissChili
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>. *)
+
+
+open Keyvalues
+
+
+let version = "0.1.0"
+
+let () =
+    print_endline ("QPC v" ^ version);
+    print_keyvalues (test "hello world");
